370b040c510268bd18d5d7d7957c0d521e87e54f
Christopher Beltran <christod.beltran@gmail.com>
2018-11-25 02:36:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end 
   
   post '/puppy' do
-    @pup = Puppy.new(params[:name], params[:])
+    @pup = Puppy.new(params[:name], params[:breed], params[:months_old])
     erb :display_puppy
   end 
     
